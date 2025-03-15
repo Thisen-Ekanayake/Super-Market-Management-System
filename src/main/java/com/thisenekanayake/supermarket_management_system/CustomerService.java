e57d330ebe14f8dc08e829c4f47381d09211ba0f
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-03-16 03:23:43 +0530

    minor fixes

--- src/main/java/com/thisenekanayake/supermarket_management_system/CustomerService.java
+++ src/main/java/com/thisenekanayake/supermarket_management_system/CustomerService.java
@@ -4,12 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.swing.*;
+import java.util.Optional;
 
 @Service
 public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    public Optional<CustomerEntity> findCustomerByMobile(String mobile_no) {
+        return customerRepository.findByMobileNumber(mobile_no);
+    }
 
     public CustomerEntity registerCustomer(CustomerEntity customer) {
         return customerRepository.save(customer); // save customer to the database
