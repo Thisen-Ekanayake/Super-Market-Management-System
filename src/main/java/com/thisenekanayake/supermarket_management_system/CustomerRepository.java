1163ba7fcc012e5539e647b81041b37bfdc5cddb
Thisen Ekanayake <thisenekanayake0330@gmail.com>
2025-03-16 03:18:09 +0530

    add find by mobile number

--- src/main/java/com/thisenekanayake/supermarket_management_system/CustomerRepository.java
+++ src/main/java/com/thisenekanayake/supermarket_management_system/CustomerRepository.java
@@ -3,6 +3,9 @@ package com.thisenekanayake.supermarket_management_system;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+    Optional<CustomerEntity> findByMobileNumber(String mobile_no);
 }
